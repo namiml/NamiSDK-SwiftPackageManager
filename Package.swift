24e273f24095ab1f12755274c6b844ec77a0e19c
Kendall Helmstetter Gelner <Kendall.Gelner@kigisoftware.com>
2022-05-20 16:49:37 -0600

    3.0.0-alpha.02

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Nami is a binary XCFramework
 	.binaryTarget(
             name: "Nami",
-            url: "https://packages.namiml.com/NamiSDK/Apple/2.9.4/Nami.xcframework-2.9.4.zip",
-            checksum: "f21cde3dbc4b706e057f4b15a4c2df4949b896d4100e5af107a78321267038c7"
+            url: "https://packages.namiml.com/NamiSDK/Apple/3.0.0-alpha.02/Nami.xcframework-3.0.0-alpha.02.zip",
+            checksum: "1b41716242e35c54df498a3bdf27bf8064b10d02d2c147cae73dacd59b60ad88"
         )
     ]
 )
